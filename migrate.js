7930baebf4fcf98fcd7a18c345d8852540d3c521
Brian <bj.johnson@oracle.com>
2025-07-13 01:51:54 -0400

    updated migrate.js file

--- migrate.js
+++ migrate.js
@@ -1,30 +1,3 @@
-
-// const { MongoClient, ServerApiVersion } = require('mongodb');
-// const uri = "mongodb+srv://johnson_8727:7j1kE7fumQWXjrol@cluster0.klmuljj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-
-// // Create a MongoClient with a MongoClientOptions object to set the Stable API version
-// const client = new MongoClient(uri, {
-//   serverApi: {
-//     version: ServerApiVersion.v1,
-//     strict: true,
-//     deprecationErrors: true,
-//   }
-// });
-
-// async function run() {
-//   try {
-//     // Connect the client to the server	(optional starting in v4.7)
-//     await client.connect();
-//     // Send a ping to confirm a successful connection
-//     await client.db("admin").command({ ping: 1 });
-//     console.log("Pinged your deployment. You successfully connected to MongoDB!");
-//   } finally {
-//     // Ensures that the client will close when you finish/error
-//     await client.close();
-//   }
-// }
-// run().catch(console.dir);
-
 const { MongoClient } = require('mongodb');
 
 const localUri = 'mongodb://localhost:27017';
